3523d47ecfb2c8a26fd085cd6bfe94de643247d4
SonuR12 <sonurai7011@gmail.com>
2025-07-02 18:43:21 +0530

    metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import InvoiceForm from '@/components/ui/form/form'
 import React from 'react'
 
+const metadata = {
+  title: 'Invoice Generator',
+  description: 'Generating invoices made easy with Next.js and TypeScript',
+}
 
 const page = () => {
   return (
